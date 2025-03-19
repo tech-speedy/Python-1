3b2087652916bfe6fcdf5c34638666febdf4afba
Arnab Sarkar <sarkararnab088@gmail.com>
2025-03-19 15:52:43 +0530

    Age-greater-than-18-or-not.py

---
+++ Age-greater-than-18-or-not.py
@@ -0,0 +1,8 @@
+#Program to check if the age of a person is greater than 18 or not
+
+a = int(input("Enter person's age: "))
+
+if a >= 18:
+  print("Person's age is greater than 18")
+else:
+  print("Person's age is not greater than 18")
