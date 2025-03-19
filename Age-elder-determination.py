7e8d2f5d94671e6a62aa4fe043fd48b0dda47331
Arnab Sarkar <sarkararnab088@gmail.com>
2025-03-19 15:43:49 +0530

    Age-elder-determination.py

---
+++ Age-elder-determination.py
@@ -0,0 +1,9 @@
+#Program to check two person's age and to determine who is elder
+
+a = int(input("Enter first person's age: "))
+b = int(input("Enter second person's age: "))
+
+if a > b:
+  print("First person's age is elder")
+else:
+  print("Second person's age is elder")
